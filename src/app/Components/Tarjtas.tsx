33e44088bef7c729288985130c94e32042f80660
diegofunes20001 <166874183+diegofunes20001@users.noreply.github.com>
2025-08-01 22:00:01 -0600

    T-clasw

--- src/app/Components/Tarjtas.tsx
+++ src/app/Components/Tarjtas.tsx
@@ -78,7 +78,7 @@ export default function Tarjtas() {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {personas.map((persona) => (
-            <Tarjtas key={persona.id} person={persona} />
+            <Tarjtas key={persona.id} Tarjtas={persona} />
           ))}
         </div>
       </div>
